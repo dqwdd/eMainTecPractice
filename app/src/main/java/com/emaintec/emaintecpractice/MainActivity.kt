a9159c73ae0ef0a59718c013db6117fceade7075
dqwdd <dqwdd@naver.com>
2021-10-10 11:38:42 +0900

    바인딩 설정해주고 자잘한 라이브러리 미리 추가해주고 main액티비티 바인딩 추가 BaseActivity.kt 추가 메인레이아웃 layout으로 감쌈

--- app/src/main/java/com/emaintec/emaintecpractice/MainActivity.kt
+++ app/src/main/java/com/emaintec/emaintecpractice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.emaintec.emaintecpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.emaintec.emaintecpractice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvent()
+        setValues()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValues() {
     }
 }
